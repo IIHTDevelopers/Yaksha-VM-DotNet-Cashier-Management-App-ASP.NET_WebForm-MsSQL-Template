1e1f84ed7d49070ff0ff0772fd8248bf33a9b6f3
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-02 12:23:15 +0530

    Add files via upload

---
+++ CashierManagementApp/DAL/Services/CashierRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using CashierManagementApp.DAL.Interfaces;
+
+namespace CashierManagementApp.DAL.Services
+{
+    public class CashierRepository : Interfaces.ICashierRepository
+    {
+        private CashierDbContext _context;
+
+        public CashierRepository(CashierDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.CashierModel GetById(string id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CashierManagementApp/DAL/Services/CashierService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using CashierManagementApp.DAL.Interfaces;
+using CashierManagementApp.Model;
+
+namespace CashierManagementApp.DAL.Services
+{
+    public class CashierService : Interfaces.ICashierService
+    {
+        private Interfaces.ICashierRepository _repository;
+
+        public CashierService(Interfaces.ICashierRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public string GetAll()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Add()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Update()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public string Delete()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public CashierModel GetById(string id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
